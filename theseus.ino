03af9f3d8fc6d45b61fffa289bf8b6dced86f4c0
bigjoe2000 <gciarami@gmail.com>
2017-03-22 13:40:04 -0400

    Defined the pinouts

---
+++ theseus.ino
@@ -0,0 +1,12 @@
+
+
+// Define which pins each of our sensors and actuators are connected to
+#define PIN_PING_TRIGGER_RIGHT 3
+#define PIN_PING_ECHO_RIGHT 4
+#define PIN_PING_TRIGGER_LEFT 5
+#define PIN_PING_ECHO_LEFT 6
+#define PIN_LED 8
+#define PIN_SERVO_LEFT 9
+#define PIN_SERVO_RIGHT 10
+#define PIN_SPEAKER 11
+#define PIN_PHOTO A0
