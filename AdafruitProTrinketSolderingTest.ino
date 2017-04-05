b6779973932bff1e2f90e65cf557bd88cea4cbd8
bigjoe2000 <gciarami@gmail.com>
2017-04-05 16:50:39 -0400

    Dumb the test script down as serial monitor is not working

--- AdafruitProTrinketSolderingTest.ino
+++ AdafruitProTrinketSolderingTest.ino
@@ -1,25 +1,20 @@
 /**
-  Set up an Adafruit Pro Trinket with the following digital pins connected (pins 2 and 7 are used for bootloader).
+  Set up an Adafruit Pro Trinket 
 
-  0 - 19
-  1 - 18
-  3 - 17
-  4 - 16
-  5 - 15
-  6 - 14
-  8 - 13
-  9 - 12
-  10- 11
+  On a breadboard, set up a resistor and LED in series with the GND pin of your trinket.
+  
+  Run a jumper cable from the LED to one of the input pins.
 
+  
+  The LED should blink only once per cycle (each cycle is denoted by the onboard LED blinking)
+  
+  Move the jumper cable to each of the pins, verifying that each one blinks once.
+  
+  Input pins are   RX,TX,3,4,5,6,8,9,10,11,12,13,A0,A1,A2,A3,A4,A5
+  
   Here's a diagram of the pin locations
 
   https://learn.adafruit.com/introducing-pro-trinket/pinouts
-  Now run this sketch and watch the serial port on 9600 bps. You'll get an alert for any pin not correctly soldered.
-
-  In case your serial monitor doesn't work, when an error is detected with a pin, the LED on the trinket will
-  blink N times, followed by 3 seconds of being off.
-  N indicates the number of the pin plus 1 (so the 0th pin can be identified)
-
 */
 
 // We check for IO on pins 0 to 19, skipping pins 2 and 7
@@ -29,60 +24,16 @@ int length = sizeof(PINS) / sizeof(int);
 int errorPin = -1;
 
 void setup() {
-  Serial.begin(9600);      // open the serial port at 9600 bps:
-  setAllInput();
   for (int i = 0; i < length; i++) {
-    validatePinValue(PINS[i], LOW);
+    pinMode(PINS[i], OUTPUT);
+    digitalWrite(PINS[i], LOW);
   }
-  Serial.println('Validated all LOW');
 }
 
 void loop() {
-  errorPin = -1;
   for (int i = 0; i < length; i++) {
-    setAllInput();
-    pinMode(PINS[i], OUTPUT);
     digitalWrite(PINS[i], HIGH);
-    for (int j = 0; j < length; j++) {
-      if (j == 1) {
-      } else if (j == length - 1) {
-        validatePinValue(PINS[j], HIGH);
-      } else {
-        validatePinValue(PINS[j], LOW);
-      }
-    }
-    Serial.print('Validated pin ');
-    Serial.println(i);
-    Serial.flush();
+    delay(200);
+    digitalWrite(PINS[i], LOW);
   }
-  if (errorPin > -1) {
-    blinkNTimes(errorPin);
-  }
-}
-
-void setAllInput() {
-  for (int i = 0; i < length; i++) {
-    pinMode(PINS[i], INPUT);
-  }
-}
-
-void validatePinValue(int pinNumber, int value) {
-  if (digitalRead(pinNumber) != value) {
-    errorPin = pinNumber;
-    Serial.print('ALERT*** Digital Pin ');
-    Serial.print(pinNumber);
-    Serial.print(' is not set to ');
-    Serial.print(value);
-    Serial.println('!!');
-  }
-}
-
-void blinkNTimes(int n) {
-  for (int i = -1; i < n; i++) {
-    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-    delay(500);                       // wait for a second
-    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-    delay(500);                       // wait for a second
-  }
-  delay(3000); // wait 3 seconds
 }
